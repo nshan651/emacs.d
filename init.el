cc28996939f8ffe301edb233a5a63c94f99eb00d
nshan651 <nickesg44@gmail.com>
2023-03-12 14:09:42 -0500

    Adding colorscheme

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 
 (menu-bar-mode -1)            ; Disable the menu bar
 
+
 ;;; Package list
 (setq package-archives 
       '(("melpa" . "https://melpa.org/packages/")
@@ -21,6 +22,18 @@
   (package-refresh-contents)
   (package-install 'use-package))
 (eval-when-compile (require 'use-package))
+
+;;; Packages
+
+;;; Color theme
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  ;; (load-theme 'doom-one t))
+  (load-theme 'doom-Iosvkem t))
 
 ;;; Undo 
 ;; Vim style undo not needed for emacs 28
@@ -45,3 +58,16 @@
   :config
   (setq evil-want-integration t)
   (evil-collection-init))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(doom-themes evil-collection evil undo-fu use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
