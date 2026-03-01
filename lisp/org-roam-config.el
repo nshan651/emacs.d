b239015b36a941ec1459c9408c55257176e0441f
nshan651 <nickesg44@gmail.com>
2026-03-01 12:35:47 -0600

    Fixed issues with consult-org-roam use-package def.
    1. (require 'consult-org-roam) inside its own use-package block is redundant - use-package already handles loading the package.
    2. (consult-org-roam-mode 1) in :init fails. The package hasn't loaded yet at that point, so the function doesn't exist. Moving it to :config fixes this.

--- lisp/org-roam-config.el
+++ lisp/org-roam-config.el
@@ -14,10 +14,6 @@
 
 (use-package consult-org-roam
  :after org-roam
- :init
- ;; (require 'consult-org-roam)
- ;; Activate the minor mode
- (consult-org-roam-mode 1)
  :custom
  ;; Use `ripgrep' for searching with `consult-org-roam-search'
  (consult-org-roam-grep-func #'consult-ripgrep)
@@ -27,10 +23,11 @@
  ;; in consult-buffer (and not down at the bottom)
  (consult-org-roam-buffer-after-buffers t)
  :config
+ (consult-org-roam-mode 1)
  ;; Eventually suppress previewing for certain functions
- ;; (consult-customize
- ;;  consult-org-roam-forward-links
- ;;  :preview-key "M-.")
+ (consult-customize
+  consult-org-roam-forward-links
+  :preview-key "M-.")
  :bind
  ;; Define some convenient keybindings as an addition
  ("C-c n e" . consult-org-roam-file-find)
